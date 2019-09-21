4a9957aa00a4f8a03bfa723793d7543b1f903419
Surabhi Lodha <surabhil@usc.edu>
2019-09-20 17:43:46 -0700

    add net and cpu balance

--- models/balances.h
+++ models/balances.h
@@ -4,8 +4,10 @@ namespace balances {
     
     struct [[eosio::table, eosio::contract("createbridge")]] contributors {
         name contributor;
-        asset balance;
+        asset balance; // used to pay for ram
         int ram;   // percentage of ram cost the contributor wants to fund
+        asset net_balance;
+        asset cpu_balance;
         int totalaccounts; // specify the limit for the number of accounts to be created by the given contribution 
         int createdaccounts; // number of accounts created using the contributor's funds
     };
